243715a94ac7f7471f0e1ba8586b5c8b11f09321
George Atkinson <georgeatkinsonwd@gmail.com>
2023-07-26 16:13:06 +0100

    changed listening for backend host

--- server.js
+++ server.js
@@ -8,19 +8,40 @@ import {postsRouter} from './routes/posts.js'
 
 const app = express()
 
+const connectDB = async () => {
+  try{
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  }
+  catch(error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
 app.use(express.json())
 app.use(cors())
 
 app.use("/auth", userRouter)
 app.use("/posts", postsRouter)
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+
+
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => {
+//     // listen for requests
+//     app.listen(process.env.PORT, () => {
+//       console.log('connected to db & listening on port', process.env.PORT)
+//     })
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+      console.log("listening for requests");
   })
-  .catch((error) => {
-    console.log(error)
-  })
+})
